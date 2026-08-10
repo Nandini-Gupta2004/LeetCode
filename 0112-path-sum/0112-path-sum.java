c58669ba4e0481abbd5d0d90f71f01c890d00af9
Nandini Gupta <nandinigarg583@gmail.com>
2026-08-10 11:43:51 +0530

    Time: 0 ms (100%), Space: 45 MB (31.89%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,18 @@
+ class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        // Leaf node
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        targetSum -= root.val;
+
+        return hasPathSum(root.left, targetSum)
+            || hasPathSum(root.right, targetSum);
+    }
+}

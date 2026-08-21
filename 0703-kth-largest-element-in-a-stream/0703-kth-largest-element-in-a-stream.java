ad2c93812e6511a3c0636f6c92c902bcd0b174a0
Nandini Gupta <nandinigarg583@gmail.com>
2026-08-21 16:05:32 +0530

    Time: 23 ms (63.76%), Space: 52.4 MB (22.26%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class KthLargest {
+
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+
+        pq.add(val);
+
+        if (pq.size() > k) {
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+}

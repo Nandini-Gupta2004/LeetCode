72395ea4719ee140df7ebb73d4ebee1d5d1f72d3
Nandini Gupta <nandinigarg583@gmail.com>
2026-08-25 15:35:20 +0530

    Time: 7 ms (72.74%), Space: 49.5 MB (84.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> mp=new HashMap<>();
+        for(String str:strs){
+            char[] ch=str.toCharArray();
+            Arrays.sort(ch);
+            String s=new String(ch);
+            if(!mp.containsKey(s)){
+                mp.put(s,new ArrayList<>());
+            }
+            mp.get(s).add(str);
+        }
+           return new ArrayList<>(mp.values());
+    }
+}

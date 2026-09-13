b3da4649ecf030c52242a3f05273ea45d620f729
Nandini Gupta <nandinigarg583@gmail.com>
2026-09-14 00:01:37 +0530

    Time: 2 ms (91.7%), Space: 47.5 MB (93.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0;
-        int j=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0)
+            nums[i++]=nums[j];
+        }
         while(i<nums.length){
-            if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-                j++;
-            } else{
-                i++;
-            }
+            nums[i++]=0;
         }
     }
 }

802edade0192ba11fe761ebc0d5e7cf8d0e001bd
Nandini Gupta <nandinigarg583@gmail.com>
2026-09-14 23:48:59 +0530

    Time: 3 ms (85.89%), Space: 43.5 MB (25.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2!=0) return false;
+        if(s.length()%2 !=0) return false;
         Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' ||s.charAt(i)=='['){
+        for(char ch:s.toCharArray()){
+            if(ch=='{' || ch=='(' || ch=='['){
                 st.push(ch);
-            } else {
-                if(st.isEmpty()) return false;
-                 char top=st.peek();
-                 if(top=='(' && ch==')') st.pop();
-                 else if(top=='{' && ch=='}') st.pop();
-                  else if(top=='[' && ch==']') st.pop();
-                  else return false;
+            } else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if((ch=='}' && st.peek()=='{') || (ch==']' && st.peek()=='[') || (ch==')' && st.peek()=='(')
+                ) st.pop();
+                else return false;
             }
         }
-
-    return st.isEmpty();
-
-     }
+        return st.isEmpty();
+    }
 }
